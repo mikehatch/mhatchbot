1b210086f570f6bb864bc1d2b6f75cb83ce03231
Mike Hatch <mihatch@reicorpnet.com>
2016-10-12 16:59:02 -0700

    Update app with goobye dialog, and LUIS integration for ManageProfile intent

--- app.js
+++ app.js
@@ -17,21 +17,35 @@ var connector = new builder.ChatConnector({
 	appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
-var intents = new builder.IntentDialog();
+//var intents = new builder.IntentDialog();
 server.post('/api/messages', connector.listen());
+
+// LUIS model
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=2ac782de-b92c-4068-a8d3-ffe8fe85de25&subscription-key=21b9f3214c2743c2b7f803bbbda93750';
+var recognizer = new builder.LuisRecognizer(model);
+var intents = new builder.IntentDialog({recognizers: [recognizer]});
+
 
 //===============
 // Bot Dialogs
 //===============
 bot.dialog('/', intents);
-intents.matches(/^change name/i, [
+
+intents.matches('ManageProfile', [
 	function(session) {
+		session.send('It sounds like you want to update your profile.');
 		session.beginDialog('/profile');
 	},
 	function(session, results) {
 		session.send('Ok... Changed your name to %s', session.userData.name);
 	}
 ]);
+
+intents.matches(/^goodbye/i, [
+	function(session) {
+		session.beginDialog('/goodbye');
+	}
+])
 intents.onDefault([
 		function(session, args, next) {
 			if(!session.userData.name) {
@@ -41,13 +55,21 @@ intents.onDefault([
 			}
 		},
 		function (session, results) {
-			session.send('Hello %s!', session.userData.name);
+				session.send('Hello %s!', session.userData.name);
 		}
 ]);
 
+bot.dialog('/goodbye', [
+	function(session) {
+		session.send('So long %s. <sniff>', session.userData.name);
+		session.userData.name = null;
+		session.endDialog();
+	}
+])
+
 bot.dialog('/profile', [
 	function(session) {
-		builder.Prompts.text(session, "Hi! What is your name?");
+		builder.Prompts.text(session, "What is your name?");
 	},
 	function(session, results) {
 		session.userData.name = results.response;  //
